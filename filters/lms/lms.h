eaaba0b26ec65c164d3941558a0f89dc4c0603f1
Tom Pacino <tompacino@users.noreply.github.com>
2024-07-17 17:55:43 +1000

    Corrected error in LMS filtering algorithm.

--- filters/lms/lms.h
+++ filters/lms/lms.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <algorithm>
 
 // TODO:
 /*
@@ -11,15 +12,16 @@
 namespace LMS
 {
     template<typename DT>
-    struct Debug
+    struct Output
     {
         DT errorSignal;
         DT currentStepSize;
     };
 
-    template<typename DT, std::size_t Taps, bool OutputDebug>
+    template<typename DT, std::size_t Taps>
     class VSS
     {
+
         DT initialStepSize;
         DT stepSize;
         DT alpha;
@@ -37,57 +39,66 @@ namespace LMS
     public:
         VSS(DT initialStepSize, DT alpha, DT gamma) : initialStepSize(initialStepSize), stepSize(initialStepSize), alpha(alpha), gamma(gamma)
         {
-            minStepSize = stepSize / 100;
-            maxStepSize = stepSize * 100;
+            minStepSize = initialStepSize / 10;
+            maxStepSize = initialStepSize * 10;
             resetTaps(0);
             resetHistory();
         };
 
-        DT step(DT xNext, DT dNext)
+        LMS::Output<DT> step(DT xNext, DT dNext)
         {
 
-            std::size_t idxStop = idxSample + 1;
-            if (idxStop >= Taps)
-            {
-                idxStop = 0;
-            }
-
             // Compute next sample estimate, error, and power
-            DT errorSignal = d_hat(0);
+            DT errorSignal = d_hat[0];
             DT power = 0;
 
             for (auto idx = 0; idx < Taps; idx++)
             {
-                errorSignal -= x_hat(idx) * h_hat(idx);
-                power += x_hat(idx) * x_hat(idx);
+                errorSignal -= x_hat[idx] * h_hat[idx];
+                power += x_hat[idx] * x_hat[idx];
             }
 
+            // std::cout << "errorSignal " << errorSignal << "\n";
+            // std::cout << "power " << power << "\n";
+
             // Update filter taps based on error
-            for (auto idx = Taps - 1; idx > 0; idx++)
+            for (auto idx = (Taps - 1); idx > 0; idx--)
             {
-                DT estimator = stepSize * errorSignal * x_hat(idx);
+                DT estimator = stepSize * errorSignal * x_hat[idx - 1];
                 estimator /= power;
-                h_hat(idx) = h_hat(idx) + estimator;
-                x_hat(idx) = x_hat(idx - 1);
-                d_hat(idx) = d_hat(idx - 1);
+                if (estimator != estimator)
+                        estimator = 0;
+                h_hat[idx] = h_hat[idx - 1] + estimator;
+                x_hat[idx] = x_hat[idx - 1];
+                d_hat[idx] = d_hat[idx - 1];
             }
-            DT estimator = stepSize * errorSignal * x_hat(0);
+            DT estimator = stepSize * errorSignal * x_hat[0];
             estimator /= power;
-            h_hat(0) = h_hat(0) + estimator;
-            x_hat(0) = xNext;
-            d_hat(0) = dNext;
+            if (estimator != estimator)
+                estimator = 0;
+            h_hat[0] = h_hat[0] + estimator;
+            x_hat[0] = xNext;
+            d_hat[0] = dNext;
+
+            // std::cout << "estimator " << estimator << "\n";
+            // std::cout << "power " << power << "\n";
+
+            // std::cout << "filterTaps:\n";
+            // std::for_each(h_hat.begin(), h_hat.end(), [](auto val)
+            //               { std::cout << val << " "; });
+
+            // std::cout << "\nxhat:\n";
+            // std::for_each(x_hat.begin(), x_hat.end(), [](auto val)
+            //               { std::cout << val << " "; });
+
+            // std::cout << "\n";
 
             // Update step size
             stepSize = alpha * stepSize + gamma * errorSignal;
+            stepSize = std::max(stepSize, minStepSize);
+            stepSize = std::min(stepSize, maxStepSize);
 
-            if (OutputDebug)
-            {
-                return Debug<DT>(errorSignal, stepSize);
-            }
-            else
-            {
-                return errorSignal;
-            }
+            return Output<DT>{errorSignal, stepSize};
         };
 
         void resetStepSize()
@@ -97,7 +108,7 @@ namespace LMS
 
         void resetTaps(DT tapValue)
         {
-            h_hat.fill(tapValue);
+            h_hat.fill(0);
         };
 
         void resetHistory()
